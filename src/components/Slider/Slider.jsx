ca0fc401cb29d7459993f490f38e00f5f16a9b50
beboopr <linnets-router.09@icloud.com>
2023-01-23 01:09:04 -0500

    added img, UseState, onClick, icons, scss

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -1,31 +1,41 @@
-import React from 'react'
+import React, { useState } from "react";
 import EastOutlinedIcon from "@mui/icons-material/EastOutlined";
 import WestOutlinedIcon from "@mui/icons-material/WestOutlined";
-
-const data = [
-    "https://images.pexels.com/photos/1549200/pexels-photo-1549200.jpeg?auto=compress&cs=tinysrgb&w=1600",
-    "https://images.pexels.com/photos/949670/pexels-photo-949670.jpeg?auto=compress&cs=tinysrgb&w=1600",
-    "https://images.pexels.com/photos/837140/pexels-photo-837140.jpeg?auto=compress&cs=tinysrgb&w=1600",
-  ];
+import "./Slider.scss";
 
 const Slider = () => {
-  return (
-    <div className='slider'>
-        <div className="container">
-            <img src={data[0]} alt="" />
-            <img src={data[1]} alt="" />
-            <img src={data[2]} alt="" />
-        </div>
-        <div className="icons">
-            <div className="icon">
-                <WestOutlinedIcon />
-            </div>
-            <div className="icon">
-                <EastOutlinedIcon />
-            </div>
-        </div>
-    </div>
-  )
-}
+  const [currentSlide, setCurrentSlide] = useState(0);
 
-export default Slider
+  const data = [
+    "https://images.pexels.com/photos/1034675/pexels-photo-1034675.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    "https://images.pexels.com/photos/9214286/pexels-photo-9214286.png?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    "https://images.pexels.com/photos/8310755/pexels-photo-8310755.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  ];
+
+  const prevSlide = () => {
+    setCurrentSlide(currentSlide === 0 ? 2 : (prev) => prev - 1);
+  };
+  const nextSlide = () => {
+    setCurrentSlide(currentSlide === 2 ? 0 : (prev) => prev + 1);
+  };
+
+  return (
+    <div className="slider">
+      <div className="container" style={{transform:`translateX(-${currentSlide * 100}vw)`}}>
+        <img src={data[0]} alt="" />
+        <img src={data[1]} alt="" />
+        <img src={data[2]} alt="" />
+      </div>
+      <div className="icons">
+        <div className="icon" onClick={prevSlide}>
+          <WestOutlinedIcon />
+        </div>
+        <div className="icon" onClick={nextSlide}>
+          <EastOutlinedIcon />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Slider;
